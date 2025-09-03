d354d3a8a3457f935c9a5669f8f810a7d8eeddc5
DanielNolberto <danni.nolberto25@gmail.com>
2025-09-03 16:21:54 -0600

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -58,6 +58,21 @@ app.run(["$rootScope", "$location", "$timeout", function($rootScope, $location, 
     $rootScope.$on("$routeChangeSuccess", function (event, current, previous) {
         $("html").css("overflow-x", "hidden")
 
+            document.addEventListener("DOMContentLoaded", function () {
+            console.log("Ya cargó el documento.")
+            // Enable pusher logging - don't include this in production
+            Pusher.logToConsole = true
+        
+            var pusher = new Pusher("fd4071018e972df38f9a", {
+              cluster: "us2"
+            })
+        
+            var channel = pusher.subscribe("canalProductos")
+            channel.bind("eventoProductos", function(data) {
+                alert(JSON.stringify(data))
+            })
+        })
+        
         const path = current.$$route.originalPath
 
         if (path.indexOf("splash") == -1) {
@@ -142,5 +157,6 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
     activeMenuOption(location.hash)
 })
+
 
 
